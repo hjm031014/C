32ab9f8fbd8dd66878d39c69dc8c31f615f0472f
홍정민 <womgon1045@naver.com>
2025-01-19 23:59:04 +0900

    p.153

--- 250114/aaa.c
+++ 250114/aaa.c
@@ -32,14 +32,41 @@ int main(void) {
 }
 */
 
+/*
 #include <stdio.h>
 int main() {
+	
 	int i;
 	float num = 0.0;
 
 	for (i = 0; i < 100; i++)
 		num += 0.1;
 	printf("0.1을 100번 더한 값 : %f\n", num);
+	
+
+	int a = 15;
+	int b = 20;
+	int c = a | b;
+	printf("OR = %d", c);
+
 	return 0;
 	
 }
+*/
+
+// while을 이용하여 입력 된 값의 구구단 출력
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int a, cnt=1;
+	printf("0 ~ 9까지의 숫자를 입력하시오 : ");
+	scanf("%d", &a);
+	
+	while (cnt <= 9) {
+		printf("%d X %d = %d\n", a, cnt, a * cnt);
+		cnt += 1;
+	}
+	return 0;
+}
+

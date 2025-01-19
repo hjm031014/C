d8c2faf7ff08c2b55479b2966807688304a901e6
홍정민 <womgon1045@naver.com>
2025-01-20 00:13:20 +0900

    P.155 (1) 중간 저장

--- 250114/aaa.c
+++ 250114/aaa.c
@@ -1,10 +1,10 @@
-
+/*
 // while을 이용하여 입력 된 값의 구구단 출력
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main() {
-	int a, cnt=1;		// a : 입력변수, cnt : 카운트 변수
+	int a, cnt=1;		// a : 입력변수, cnt : 카운트 변수햣
 	printf("0 ~ 9까지의 숫자를 입력하시오 : ");
 	scanf("%d", &a);
 	
@@ -14,4 +14,12 @@ int main() {
 	}
 	return 0;
 }
+*/
 
+// 양의 정수를 입력받아, 그 수만큼 Hello World! 출력 (while 사용)
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdint.h>
+int main() {
+	int b, cnt;		// b : 입력 변수, cnt : 카운트 변수
+
+}

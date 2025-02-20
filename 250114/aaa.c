e99aa95f1f69447a19ffe218e958d4a64198d6d1
wxdals <womgon1045@naver.com>
2025-02-20 18:03:28 +0900

    P.167 (2) 수정

--- 250114/aaa.c
+++ 250114/aaa.c
@@ -16,16 +16,43 @@ int main() {
 }
 */
 
+
+/*
 // 양의 정수를 입력받아, 그 수만큼 Hello World! 출력 (while 사용)
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 int main() {
 	int a, cnt = 1;		// a : 입력 변수, cnt : 카운트 변수
 	printf("양의 정수를 입력하시오 : \n");
-	//scanf("%d", &a);
+	// scanf("%d", &a);
 	a = 10;
 	while (cnt <= a) {
 		printf("Hello World\n");
 		cnt += 1;
 	}
+}
+// 155쪼ㅓㄱ
+
+*/
+
+//	두 개의 정수를 입력 받아 그 사이의 정수의 총 합 구하기 (for문) 
+//	ex) 3, 5 => 3+4+5
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int a, b, sum;
+	// int sum = 0;
+
+	printf("총 합계를 구할 두 정수를 입력하시오 : ");
+	scanf("%d", &a);
+	scanf("%d", &b);
+
+	for (sum = 0; a>=b; a++)
+	{
+		sum += a;
+	}
+
+	printf("%d", sum);
 }
